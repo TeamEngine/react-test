9907481b7fda22c9f244a2ff5453da1e688f0b7e
shakyamoksha <developer.rskmicrofinance@outlook.com>
2022-02-15 13:44:08 +0400

    added actions

--- src/redux/employees/actionCreators.js
+++ src/redux/employees/actionCreators.js
@@ -4,3 +4,11 @@ import * as actions from ".";
 export const saveNewEmployee = employee => dispatch => {
   dispatch(actions.saveNewEmployee(employee));
 };
+
+export const removeEmployee = employeeId => dispatch => {
+  dispatch(actions.removeEmployee(employeeId));
+};
+
+export const editEmployee = employee => dispatch => {
+  dispatch(actions.editEmployee(employee));
+};
